1463851fc2530ace3770a2b393e70a62b507aee1
Thomas Francart <thomas.francart@sparna.fr>
2019-12-06 13:42:33 +0100

    equivalentClass -> sh:hasValue

--- shacl-validator/src/main/java/fr/sparna/rdf/shacl/owl2shacl/Owl2Shacl.java
+++ shacl-validator/src/main/java/fr/sparna/rdf/shacl/owl2shacl/Owl2Shacl.java
@@ -1,11 +1,13 @@
 package fr.sparna.rdf.shacl.owl2shacl;
 
+import org.apache.jena.ontology.OntDocumentManager;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.util.FileManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.topbraid.shacl.rules.RuleUtil;
@@ -38,9 +40,24 @@ public class Owl2Shacl {
 	}
 	
 	public Model convert(Model input, Owl2ShaclStyle style) {
+
+//		OntDocumentManager mgr = new OntDocumentManager();
+//		// set mgr's properties now
+//		FileManager.get().addLocatorClassLoader(getClass().getClassLoader());
+//		FileManager.get().getLocationMapper().addAltEntry("http://sparna.fr/ontologies/owl2sh", "/fr/sparna/rdf/shacl/rdf2shacl/owl2shacl-common.ttl");
+//		
+//		mgr.setFileManager(FileManager.get());
+//		
+//		// now use it
+//		OntModelSpec myOntModelSpec = new OntModelSpec( OntModelSpec.OWL_MEM );
+//		myOntModelSpec.setDocumentManager( mgr );
+//		
+//		
+//		// read shapes file
+//		OntModel shapesModel = ModelFactory.createOntologyModel(myOntModelSpec);	
 		
-		// read shapes file
-		OntModel shapesModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);		
+		OntModel shapesModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+		
 		shapesModel.read(
 				this.getClass().getResourceAsStream(style.getResourcePath()),
 				null,

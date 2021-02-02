ecbb0d919b2c21b2acab0ad0095be027e6f1ec0d
Jorge AQUINO <jorge.aquino@sparna.fr>
2021-02-02 09:16:58 +0100

    Update shacl doc

--- shacl-doc/src/main/java/fr/sparna/rdf/shacl/doc/Main.java
+++ shacl-doc/src/main/java/fr/sparna/rdf/shacl/doc/Main.java
@@ -1,13 +1,18 @@
 package fr.sparna.rdf.shacl.doc;
 
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 
-import org.apache.jena.rdf.model.Model; 
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.util.FileUtils;
 import org.apache.jena.vocabulary.RDF;
+import org.topbraid.shacl.vocabulary.SH;
 
 public class Main {
 
@@ -18,5 +23,19 @@ public class Main {
 
 		shaclGraph.read(new FileInputStream(shaclFile), RDF.uri, FileUtils.guessLang(shaclFile, "RDF/XML"));
 
+		ShaclWriter writer = new ShaclWriter();
+		String output = writer.writeInsShaclxml(shaclGraph);
+		
+		String outputDirectory ="C:/Temp" ; //args[1];
+		
+		// determine output filename
+		File inputFile = new File(shaclFile);
+		String fileName = inputFile.getName();
+		
+	
+		// output raw string
+	    OutFileDocument outfile = new OutFileDocument(new File(outputDirectory));
+		outfile.outfileuml(output, fileName.substring(0, fileName.lastIndexOf('.'))+".xml");
+		
 	}
 }

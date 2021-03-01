656dcabdcb52af8782d0d239ed831425d308dbcd
Jorge AQUINO <jorge.aquino@sparna.fr>
2021-03-01 17:24:20 +0100

    Update URI link Column

--- shacl-doc/src/main/java/fr/sparna/rdf/shacl/doc/OutFilePDF.java
+++ shacl-doc/src/main/java/fr/sparna/rdf/shacl/doc/OutFilePDF.java
@@ -1,35 +1,21 @@
 package fr.sparna.rdf.shacl.doc;
 
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
+import java.io.OutputStream;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 
 
 public class OutFilePDF {
-	public void generatePDFFromHTML(String filename)
-			throws ParserConfigurationException, IOException, DocumentException {
-		Document document = new Document(PageSize.LETTER);
-		
-		// get Instance of the PDFWriter
-		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("/tmp/outputfile.pdf"));
-		//open document
-		document.open();
-		
-		//get the XMLWorkerHelper Instance
-		XMLWorkerHelper worker = XMLWorkerHelper.getInstance();
-		//convert to PDF
-		worker.parseXHtml(writer, document, new FileInputStream(filename));
-        
-		
-		document.close();
-		
-	}
+	public void generatePDFFromHTML(String filename) throws FileNotFoundException, IOException {
+		try (OutputStream os = new FileOutputStream("/tmp/out.pdf")) 
+		{
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.useFastMode();
+            builder.withUri("file:///tmp/output.html");
+            builder.toStream(os);
+            builder.run();
+        }
+    }	
 }

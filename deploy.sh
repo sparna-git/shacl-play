8e71de2c60c66170732892ffd3836fdfdc84fb1a
Thomas Francart <thomas.francart@sparna.fr>
2023-03-03 16:11:58 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 scp shacl-play/target/shacl-play-0.6.2.war admin@calliope.sparna.fr:~
 
 ssh -t admin@calliope.sparna.fr 'su -c "\
-rm -rf /var/lib/tomcat8/tomcat8-instance1/webapps/shacl-play.war
-rm -rf /var/lib/tomcat8/tomcat8-instance1/webapps/shacl-play
+rm -rf /var/lib/tomcat8/tomcat8-instance1/webapps/play.war
+rm -rf /var/lib/tomcat8/tomcat8-instance1/webapps/play
 service tomcat8-instance1 stop
-cp /home/admin/shacl-play-0.6.2.war /var/lib/tomcat8/tomcat8-instance1/webapps/ROOT.war
+cp /home/admin/shacl-play-0.6.2.war /var/lib/tomcat8/tomcat8-instance1/webapps/play.war
 service tomcat8-instance1 start"'

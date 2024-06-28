9cb4adb97916b6016c6aba7117d4fc6803f7d284
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2024-06-28 12:58:35 +0200

    add commons class

---
+++ shacl-commons/src/main/java/fr/sparna/rdf/jena/shacl/DcatDistribution.java
@@ -0,0 +1,23 @@
+package fr.sparna.rdf.jena.shacl;
+
+public class DcatDistribution {
+
+	protected String dctFormat;
+	protected String dcatURL;
+	
+	
+	public String getDctFormat() {
+		return dctFormat;
+	}
+	public void setDctFormat(String dctFormat) {
+		this.dctFormat = dctFormat;
+	}
+	public String getDcatURL() {
+		return dcatURL;
+	}
+	public void setDcatURL(String dcatURL) {
+		this.dcatURL = dcatURL;
+	}
+	
+	
+}

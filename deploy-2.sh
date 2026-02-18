9e89285b715217f835839b5abad9de823748102c
Thomas Francart <thomas.francart@sparna.fr>
2026-02-18 18:17:07 +0100

    deploy on new server

--- deploy-2.sh
+++ deploy-2.sh
@@ -1,15 +1,11 @@
-scp shacl-play/target/shacl-play-0.11.5.war ubuntu@92.243.27.145:~
+# Transfer
+scp -i /home/thomas/.ssh/sparna-keypair-francfort.pem shacl-play/target/shacl-play-0.11.5.war ubuntu@92.243.27.145:~
 
-# ssh -t admin@calliope.sparna.fr 'su -c "\
-# rm -rf /var/lib/tomcat8/tomcat8-instance1/webapps/play.war
-# rm -rf /var/lib/tomcat8/tomcat8-instance1/webapps/play
-# service tomcat8-instance1 stop
-# cp /home/admin/shacl-play-0.11.5.war /var/lib/tomcat8/tomcat8-instance1/webapps/play.war
-# service tomcat8-instance1 start"'
-
-ssh -t ubuntu@92.243.27.145 'su -c "\
-rm -rf /var/lib/tomcat9/webapps/play.war
-rm -rf /var/lib/tomcat9/webapps/play
-service tomcat9 stop
-cp /home/admin/shacl-play-0.11.5.war /var/lib/tomcat9/webapps/play.war
-service tomcat9 start"'
+# Redeploy
+ssh -i /home/thomas/.ssh/sparna-keypair-francfort.pem ubuntu@92.243.27.145 'sudo su -c "\
+service tomcat10 stop
+rm -rf /var/lib/tomcat10/webapps/play.war
+rm -rf /var/lib/tomcat10/webapps/play
+rm -rf /var/lib/tomcat10/logs/*
+cp /home/ubuntu/shacl-play-0.11.5.war /var/lib/tomcat10/webapps/play.war
+service tomcat10 start"'

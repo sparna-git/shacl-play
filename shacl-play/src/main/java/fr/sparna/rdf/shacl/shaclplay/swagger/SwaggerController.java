b874001bf36bdd92800a0ffd0070306990c33de2
ArthurL4 <arthur-leroux@hotmail.com>
2026-05-29 22:05:00 +0200

    Update SwaggerController.java

--- shacl-play/src/main/java/fr/sparna/rdf/shacl/shaclplay/swagger/SwaggerController.java
+++ shacl-play/src/main/java/fr/sparna/rdf/shacl/shaclplay/swagger/SwaggerController.java
@@ -1,6 +1,5 @@
 package fr.sparna.rdf.shacl.shaclplay.swagger;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,8 +10,6 @@ public class SwaggerController {
     public String swagger(){
         return "api-doc";
     }
-
-    @Autowired
-    private org.springdoc.core.properties.SpringDocConfigProperties springDocConfigProperties;
     
+
 }
